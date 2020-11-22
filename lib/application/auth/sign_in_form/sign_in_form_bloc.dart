16509b2242d647c45832d87415c45f20bdacb423
unknown <venkat@codinggeek.com>
2020-11-22 07:36:37 +0530

    modeling sign in form events and state

---
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:notes_app/domain/auth/auth_failure.dart';
+import 'package:notes_app/domain/auth/i_auth_facade.dart';
+import 'package:notes_app/domain/auth/value_objects.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  // Check this line, if you run into error
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
+
+  SignInFormState get initialState => SignInFormState.initial();
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
